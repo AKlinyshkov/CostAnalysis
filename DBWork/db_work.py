f9f8404d01e955e16de60763c29b6b89263e3342
AKlinyshkov <AKlinyshkov@mail.ru>
2023-08-31 14:26:48 +0400

    Add dataclasses

--- DBWork/db_work.py
+++ DBWork/db_work.py
@@ -1,61 +1,92 @@
-"""
-Errors:
-Empty date or bad format - 1
-Empty product - 3
-Empty cost/sum or bad format - 5
-Cost/sum less then zero - 6
-"""
-
 import sqlite3
+from dataclasses import dataclass
+
+
+@dataclass
+class Purchase:
+    """
+    date: str
+    product: str
+    cost: float
+    sum_: float
+    id_: int = 0
+    """
+    date: str
+    product: str
+    cost: float
+    sum_: float
+    id_: int = 0
+
+
+@dataclass
+class Category:
+    """
+    product: str
+    category: str
+    """
+    product: str
+    category: str
 
 
 class DBWork:
 
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
         self.dbconn = sqlite3.connect(path)
 
     # ==============================================================
     # Select data
     # ==============================================================
 
-    def select_purchase_item(self, id_: int) -> tuple[int, str, str, float, float] | None:
+    def select_purchase_item(self, id_: int) -> Purchase | None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute(f'SELECT * FROM purchases WHERE id = {id_}')
-            purchase = cursor.fetchone()
+            pur = cursor.fetchone()
+            purchase = Purchase(id_=pur[0], date=pur[1], product=pur[2], cost=pur[3], sum_=pur[4])
         except Exception as exp:
             raise exp
         finally:
             cursor.close()
         return purchase
 
-    def select_all_purchases(self) -> list[tuple[int, str, str, float, float]] | None:
+    def select_all_purchases(self) -> list[Purchase] | None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute('SELECT id, date(date), product, cost, sum FROM purchases')
-            all_purchases = cursor.fetchall()
+            purs = cursor.fetchall()
+            all_purchases = [
+                Purchase(
+                    id_=pur[0],
+                    date=pur[1],
+                    product=pur[2],
+                    cost=pur[3],
+                    sum_=pur[4]
+                ) for pur in purs
+            ]
         except Exception as exp:
             raise exp
         finally:
             cursor.close()
         return all_purchases
 
-    def select_all_categories(self) -> list[tuple[str, str]] | None:
+    def select_all_categories(self) -> list[Category] | None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute('SELECT * FROM categories')
-            all_categories = cursor.fetchall()
+            categories = cursor.fetchall()
+            all_categories = [Category(product=cat[0], category=cat[1]) for cat in categories]
         except Exception as exp:
             raise exp
         finally:
             cursor.close()
         return all_categories
 
-    def select_category_by_product(self, product: str) -> tuple[str, str] | None:
+    def select_category_by_product(self, product: str) -> Category | None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute(f"SELECT * FROM categories WHERE product = '{product}'")
-            category = cursor.fetchone()
+            cat = cursor.fetchone()
+            category = Category(product=cat[0], category=cat[1])
         except Exception as exp:
             raise exp
         finally:
@@ -66,12 +97,13 @@ class DBWork:
     # Insert Delete Update
     # ==============================================================
 
-    def insert_into_purchases(self, date: str, product: str, cost: float, sum_: float) -> None:
+    def insert_into_purchases(self, purchase: Purchase) -> None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute(
                 'INSERT INTO purchases (date, product, cost, sum) '
-                f"VALUES(date('{date}'), '{product}', '{cost}', '{sum_}')"
+                f"VALUES(date('{purchase.date}'), '{purchase.product}', "
+                f"'{purchase.cost}', '{purchase.sum_}')"
             )
         except Exception as exp:
             raise exp
@@ -89,16 +121,16 @@ class DBWork:
             cursor.close()
         self.dbconn.commit()
 
-    def update_purchases(self, id_: int, date: str, product: str, cost: float, sum_: float) -> None:
+    def update_purchases(self, purchase: Purchase) -> None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute(
                 'UPDATE purchases '
-                f"set date = date('{date}'), "
-                f"product = '{product}', "
-                f"cost = '{cost}', "
-                f"sum = '{sum_}' "
-                f"where id = {id_}"
+                f"set date = date('{purchase.date}'), "
+                f"product = '{purchase.product}', "
+                f"cost = '{purchase.cost}', "
+                f"sum = '{purchase.sum_}' "
+                f"where id = {purchase.id_}"
             )
         except Exception as exp:
             raise exp
@@ -106,12 +138,12 @@ class DBWork:
             cursor.close()
         self.dbconn.commit()
 
-    def insert_into_categories(self, product: str, category: str) -> None:
+    def insert_into_categories(self, category: Category) -> None:
         cursor = self.dbconn.cursor()
         try:
             cursor.execute(
                 'INSERT INTO categories(product, category) '
-                f"VALUES('{product}', '{category}')"
+                f"VALUES('{category.product}', '{category.category}')"
             )
         except Exception as exp:
             raise exp
