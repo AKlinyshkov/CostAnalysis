d39fa7eae63a5d6796e654a4589e2c527ac611b5
AKlinyshkov <AKlinyshkov@mail.ru>
2023-09-03 14:51:50 +0400

    Add abstract db class

--- DBWork/db_work.py
+++ DBWork/db_work.py
@@ -1,6 +1,5 @@
-import sqlite3
-import asyncio
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 
 @dataclass
@@ -29,125 +28,43 @@ class Category:
     category: str
 
 
-class DBWork:
-
-    def __init__(self, path: str) -> None:
-        self.dbconn = sqlite3.connect(path)
-
+class DBWork(ABC):
     # ==============================================================
     # Select data
     # ==============================================================
 
+    @abstractmethod
     async def select_purchase_item(self, id_: int) -> Purchase | None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(f'SELECT * FROM purchases WHERE id = {id_}')
-            pur = cursor.fetchone()
-            purchase = Purchase(id_=pur[0], date=pur[1], product=pur[2], cost=pur[3], sum_=pur[4])
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        return purchase
+        pass
 
+    @abstractmethod
     async def select_all_purchases(self) -> list[Purchase] | None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute('SELECT id, date(date), product, cost, sum FROM purchases')
-            purs = cursor.fetchall()
-            all_purchases = [
-                Purchase(
-                    id_=pur[0],
-                    date=pur[1],
-                    product=pur[2],
-                    cost=pur[3],
-                    sum_=pur[4]
-                ) for pur in purs
-            ]
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        return all_purchases
+        pass
 
+    @abstractmethod
     async def select_all_categories(self) -> list[Category] | None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute('SELECT * FROM categories')
-            categories = cursor.fetchall()
-            all_categories = [Category(product=cat[0], category=cat[1]) for cat in categories]
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        return all_categories
+        pass
 
+    @abstractmethod
     async def select_category_by_product(self, product: str) -> Category | None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(f"SELECT * FROM categories WHERE product = '{product}'")
-            cat = cursor.fetchone()
-            category = Category(product=cat[0], category=cat[1])
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        return category
+        pass
 
     # ==============================================================
     # Insert Delete Update
     # ==============================================================
 
+    @abstractmethod
     async def insert_into_purchases(self, purchase: Purchase) -> None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(
-                'INSERT INTO purchases (date, product, cost, sum) '
-                f"VALUES(date('{purchase.date}'), '{purchase.product}', "
-                f"'{purchase.cost}', '{purchase.sum_}')"
-            )
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        self.dbconn.commit()
+        pass
 
+    @abstractmethod
     async def delete_from_purchases(self, id_: int) -> None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(f'DELETE FROM purchases WHERE id = {id_}')
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        self.dbconn.commit()
+        pass
 
+    @abstractmethod
     async def update_purchases(self, purchase: Purchase) -> None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(
-                'UPDATE purchases '
-                f"set date = date('{purchase.date}'), "
-                f"product = '{purchase.product}', "
-                f"cost = '{purchase.cost}', "
-                f"sum = '{purchase.sum_}' "
-                f"where id = {purchase.id_}"
-            )
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        self.dbconn.commit()
+        pass
 
+    @abstractmethod
     async def insert_into_categories(self, category: Category) -> None:
-        cursor = self.dbconn.cursor()
-        try:
-            cursor.execute(
-                'INSERT INTO categories(product, category) '
-                f"VALUES('{category.product}', '{category.category}')"
-            )
-        except Exception as exp:
-            raise exp
-        finally:
-            cursor.close()
-        self.dbconn.commit()
+        pass
